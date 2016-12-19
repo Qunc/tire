3d6036187aea0e9520b0768594a3f5e531cfa0bf
qunnnnnnnnnn <329731682@qq.com>
2016-12-19 10:06:10 +0800

    CSS for user_details

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ var getQueryParam = function (paras) {
 }
 
 
-//localStorage.setItem('token','5853f1432e9ffc32b519a8c3');
+//localStorage.setItem('token','585508c42e9ffc653275d5d4');
 
 //检查是否保存有token
 var token = localStorage.getItem('token');
